8aada86a00141c4d62db3ac1a0fedc24391a06a1
VIC User <VIC User@MELT-HOLO15>
2019-11-04 13:03:35 -0500

    Add project files.

---
+++ Camera Application/MainWindow.xaml.cs
@@ -0,0 +1,143 @@
+﻿using AVT.VmbAPINET;
+using Camera_Application.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Camera_Application
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+
+            // Setup table
+            setupTable();
+
+            // Detect cameras
+            try
+            {
+                detectCameras();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.ToString());
+            }
+        }
+
+        private void setupTable()
+        {
+            var gridView = new GridView();
+            this.listView.View = gridView;
+
+            gridView.Columns.Add(new GridViewColumn
+            {
+                Header = "Camera ID",
+                DisplayMemberBinding = new Binding("cameraID")
+            });
+            gridView.Columns.Add(new GridViewColumn
+            {
+                Header = "Camera Name",
+                DisplayMemberBinding = new Binding("cameraName")
+            });
+            gridView.Columns.Add(new GridViewColumn
+            {
+                Header = "Camera Model",
+                DisplayMemberBinding = new Binding("cameraModel")
+            });
+
+            // Populate list
+            //this.listView.Items.Add(new MyItem { Id = 1, Name = "David" });
+        }
+
+        private void detectCameras()
+        {
+            Vimba sys = new Vimba();
+            CameraCollection cameras;
+
+            string strCameraID;
+            string strCameraName;
+            string strCameraModel;
+
+            try
+            {
+                sys.Startup();                      // Initialize the Vimba API
+                Console.WriteLine("Vimba .NET API Version {0:D}.{1:D}.{2:D}", sys.Version.major, sys.Version.minor, sys.Version.patch);
+                cameras = sys.Cameras;              // Fetch all cameras known to Vimba
+
+                Console.WriteLine("Cameras found: " + cameras.Count);
+                Console.WriteLine();
+
+                // Query all static details of all known cameras and print them out.
+                // We don't have to open the cameras for that.
+                foreach (Camera camera in cameras)
+                {
+                    try
+                    {
+                        strCameraID = camera.Id;
+                    }
+                    catch (VimbaException ve)
+                    {
+                        strCameraID = ve.Message;
+                    }
+
+                    try
+                    {
+                        strCameraName = camera.Name;
+                    }
+                    catch (VimbaException ve)
+                    {
+                        strCameraName = ve.Message;
+                    }
+
+                    try
+                    {
+                        strCameraModel = camera.Model;
+                    }
+                    catch (VimbaException ve)
+                    {
+                        strCameraModel = ve.Message;
+                    }
+
+                    this.listView.Items.Add(new CameraModel { cameraID = strCameraID, cameraName = strCameraName, cameraModel = strCameraModel });
+
+                    Console.WriteLine("/// Camera Name: " + strCameraName);
+                    Console.WriteLine("/// Model Name: " + strCameraModel);
+                    Console.WriteLine("/// Camera ID: " + strCameraID);
+                    Console.WriteLine();
+                }
+            }
+            finally
+            {
+                sys.Shutdown();
+            }
+        }
+
+        private void listView_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
+
+        private void button_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
---
+++ Camera Application/Models/CameraModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Camera_Application.Models
+{
+    public class CameraModel
+    {
+        public string cameraID { get; set; }
+        public string cameraModel { get; set; }
+        public string cameraName { get; set; }
+
+    }
+}
